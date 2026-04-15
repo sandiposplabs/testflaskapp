1184648c747f03f497fef8f816760e34ec119c9a
ubuntu <sandiposplabs@gmail.com>
2026-04-15 15:28:48 +0530

    changes3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask CI/CD Working!, changes to dev"
+    return "Flask CI/CD Working!, changes3"
 
 if __name__ == "__main__":
     app.run()
